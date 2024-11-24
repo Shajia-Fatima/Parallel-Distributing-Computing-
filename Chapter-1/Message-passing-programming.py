d5654578c2f4d559aaacd8a7201768777a9b3040
Shajia-Fatima <shajiafatima420@gmail.com>
2024-11-25 01:49:28 +0500

    Update Message-passing-programming.py

--- Chapter-1/Message-passing-programming.py
+++ Chapter-1/Message-passing-programming.py
@@ -1,5 +1,6 @@
 from mpi4py import MPI
 
+# Initialize MPI communication
 comm = MPI.COMM_WORLD
 rank = comm.Get_rank()  # Get the rank of the current process
 size = comm.Get_size()  # Get the total number of processes
@@ -8,11 +9,12 @@ if size < 2:
     print("This program requires at least 2 processes.")
 else:
     if rank == 0:
-        data = "Hello from rank 0"
-        comm.send(data, dest=1)  # Send data to process with rank 1
-        print(f"Process {rank} sent data to process 1.")
+        # Changed the data to something different
+        data_to_send = {'Name': 'Alice', 'Age': 25, 'City': 'New York'}
+        comm.send(data_to_send, dest=1)
+        print("Process 0: Sent data to process 1")
     elif rank == 1:
-        data = comm.recv(source=0)  # Receive data from process with rank 0
-        print(f"Process {rank} received data: {data}")
+        received_data = comm.recv(source=0)  # Receive data from process 0
+        print(f"Process 1: Received data - {received_data}")
     else:
-        print(f"Process {rank} is idle.")  # Print message for idle processes
+        print(f"Process {rank}: No operations assigned. Remaining idle.")
