49516b2418651ef5eb841a2c5d9edf8c7c49cac7
Shajia-Fatima <shajiafatima420@gmail.com>
2025-01-02 00:07:47 +0500

    commit

--- Chapter-2/Thread-name&processes.py
+++ Chapter-2/Thread-name&processes.py
@@ -1,35 +1,29 @@
-from threading import Thread
-import time
 import os
+from threading import Thread
 
-class MyThreadClass (Thread):
-   def __init__(self, name):
-      Thread.__init__(self)
-      self.name = name
+class MyThreadClass(Thread):
+    def __init__(self, name):
+        Thread.__init__(self)
+        self.name = name
  
-   def run(self):
-       print("ID of process running {}".format(self.name)) #, " is {} \n".format(os.getpid()))
+    def run(self):
+        print(f"ID of thread running {self.name} is {os.getpid()}")
 
 def main():
-    from random import randint
-    # Thread Creation
-    thread1 = MyThreadClass("Thread#1 ")
-    thread2 = MyThreadClass("Thread#2 ")
+    # Thread creation
+    thread1 = MyThreadClass("Thread#1")
+    thread2 = MyThreadClass("Thread#2")
   
-    # Thread Running
+    # Thread starting
     thread1.start()
     thread2.start()
-
 
     # Thread joining
     thread1.join()
     thread2.join()
 
-    # End 
-    print("End")
-
+    # End
+    print("End of main program.")
 
 if __name__ == "__main__":
     main()
-
-    
